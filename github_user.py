afbf1ae2afb74cf01b474b17fd5c07e924556e4f
Ov7070 <merident00773k.a@gmail.com>
2026-09-24 00:20:20 +0330

    Update github_user.py

--- github_user.py
+++ github_user.py
@@ -1,16 +1,24 @@
 import requests as r
 
-user=input("user name : ")
+user = input("user name : ")
 
-add=f"https://api.github.com/users/{user}"
+add = f"https://api.github.com/users/{user}"
 
-end=r.get(add )
+end = r.get(add)
+
 print(end.status_code)
 
-data=end.json()
+if end.status_code == 200:
+    data = end.json()
 
-print(f"login  :{data['login']}")
-print(f"name :{data['name']}")
-print(f"followers :{data['followers']}")
-print(f"following :{data['following']}")
-print(f"public repos : {data['public_repos']}")
+    print(f"login : {data['login']}")
+    print(f"name : {data['name']}")
+    print(f"followers : {data['followers']}")
+    print(f"following : {data['following']}")
+    print(f"public repos : {data['public_repos']}")
+
+elif end.status_code == 404:
+    print("User not found")
+
+else:
+    print(f"Error: {end.status_code}")
